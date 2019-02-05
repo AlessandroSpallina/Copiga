c304e39c79f26332645b6619461888a6f5527db4
= <alessandrospallina1@gmail.com>
2019-02-05 10:40:22 +0000

    w

--- src/cliente/copi.ga/resources/views/welcome.blade.php
+++ src/cliente/copi.ga/resources/views/welcome.blade.php
@@ -13,58 +13,44 @@
         <!-- Styles -->
         <style>
             html, body {
-
-                background-color: #0099cc;
-                #color: #636b6f;
-
+                background-color: #fff;
+                color: #636b6f;
                 font-family: 'Nunito', sans-serif;
-                font-weight: 900;
-                height: 500vh;
+                font-weight: 200;
+                height: 100vh;
                 margin: 0;
             }
-
-            sfondo {
-              background-image: url("/img/scrivania.jpg");
-            }
-
             .full-height {
                 height: 100vh;
             }
-
             .flex-center {
                 align-items: center;
                 display: flex;
                 justify-content: center;
             }
-
             .position-ref {
                 position: relative;
             }
-
             .top-right {
                 position: absolute;
                 right: 10px;
                 top: 18px;
             }
-
             .content {
                 text-align: center;
             }
-
             .title {
                 font-size: 84px;
             }
-
             .links > a {
-                color: #ffffff;
+                color: #636b6f;
                 padding: 0 25px;
-                font-size: 12px;
+                font-size: 13px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
             .m-b-md {
                 margin-bottom: 30px;
             }
