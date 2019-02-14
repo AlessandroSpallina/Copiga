0bd8e1f31937f0dd6da33b813b31c898d529390b
manlio <manpuglisi94@gmail.com>
2019-02-14 19:38:59 +0100

    work

--- src/esercente/CopigaEs/src/ProvaEsercente.java
+++ src/esercente/CopigaEs/src/ProvaEsercente.java
@@ -1,3 +1,4 @@
+
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
@@ -14,19 +15,28 @@ import javax.swing.UIManager;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableCellRenderer;
 
-public class ButtonExample {
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-/*    public static void main(String[] args) {
-        final ButtonExample example = new ButtonExample();
+/**
+ *
+ * @author manlio
+ */
+public class ProvaEsercente {
+    public static void main(String[] args) {
+        final ProvaEsercente ejemplo = new ProvaEsercente();
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                example.createAndShowGUI();
+                ejemplo.createAndShowGUI();
             }
         });
-    }*/
+    }
 	
-    void createAndShowGUI() {
-        JFrame frame = new JFrame("Button Example");
+    private void createAndShowGUI() {
+        JFrame frame = new JFrame("Prova Esercente");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         JTable table = new JTable(new JTableModel()); 
@@ -34,20 +44,21 @@ public class ButtonExample {
 	table.setFillsViewportHeight(true);	
 		
 	TableCellRenderer buttonRenderer = new JTableButtonRenderer();
-	table.getColumn("Button1").setCellRenderer(buttonRenderer);
-	table.getColumn("Button2").setCellRenderer(buttonRenderer);
+	table.getColumn("Visualizza File").setCellRenderer(buttonRenderer);
+	table.getColumn("Accetta File").setCellRenderer(buttonRenderer);
+        table.getColumn("Notifica Ritiro").setCellRenderer(buttonRenderer);
         table.addMouseListener(new JTableButtonMouseListener(table));
         
         frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-        frame.getContentPane().setPreferredSize(new Dimension(500, 200));
+        frame.getContentPane().setPreferredSize(new Dimension(800, 400));
         frame.pack();
         frame.setVisible(true);
     }
 	
     public static class JTableModel extends AbstractTableModel {
 	private static final long serialVersionUID = 1L;
-	private static final String[] COLUMN_NAMES = new String[] {"Id", "Stuff", "Button1", "Button2"};
-	private static final Class<?>[] COLUMN_TYPES = new Class<?>[] {Integer.class, String.class, JButton.class,  JButton.class};
+	private static final String[] COLUMN_NAMES = new String[] {"File", "Cliente", "Visualizza File", "Accetta File", "Notifica Ritiro"};
+	private static final Class<?>[] COLUMN_TYPES = new Class<?>[] {Integer.class, String.class, JButton.class,  JButton.class,  JButton.class};
 	
 	@Override public int getColumnCount() {
             return COLUMN_NAMES.length;
@@ -67,17 +78,19 @@ public class ButtonExample {
 	}
 
 	@Override public Object getValueAt(final int rowIndex, final int columnIndex) {
+            // a seconda dell'indice della colonna fa qualcosa
             switch (columnIndex) {
 		case 0: return rowIndex;
 		case 1: return "Text for "+rowIndex;
 		case 2: // fall through
-		case 3: final JButton button = new JButton(COLUMN_NAMES[columnIndex]);
-		button.addActionListener(new ActionListener() {
+		case 3: final JButton button1 = new JButton(COLUMN_NAMES[columnIndex]);
+		button1.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent arg0) {
-			JOptionPane.showMessageDialog(JOptionPane.getFrameForComponent(button), 
+			JOptionPane.showMessageDialog(JOptionPane.getFrameForComponent(button1), 
 				"Button clicked for row "+rowIndex);
                     }
 		});
+                case 4: final JButton button = new JButton(COLUMN_NAMES[columnIndex]);
 		return button;
 		default: return "Error";
             }
